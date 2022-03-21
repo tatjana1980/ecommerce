fcedfc4c36dfbcad22cf79452865e81a94760f5e
tatjana1980 <90955635+tatjana1980@users.noreply.github.com>
2022-03-21 20:06:21 +0300

    add user register

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
 require('dotenv').config({
     path: './config/index.env'
 })
+//MongoDB
+const connectDB = require('./config/db');
+connectDB();
 
-app.use(express.urlencoded({ extended: true }));
+
 app.use(morgan('dev'));
 app.use(cors());
+
+// routes
+app.use('/api/user/', require('./routes/auth.route'));
 
 app.get('/', (req, res) => {
     res.send('test route => home page');
